e1f99bc5ae5de9707fc8bb8599e35681b0700e44
jaRue1 <jaruejohnson@outlook.com>
2021-05-04 18:07:21 -0400

    fixed class react app example

--- src/App.js
+++ src/App.js
@@ -10,19 +10,41 @@ function App() {
       .then((promise) => setAllRestaurants(promise))
       .catch((err) => console.log(err))
   }, [])
-  // console.log(allRestaurants)
-  function handleSubmit(){
+  function handleSubmit() {
     console.log("Your have Handled your Submit: ", text)
   }
+  function sendRestaurant(event) {
+    event.preventDefault()
 
+    const newRestaurants = {
+      name: "Carrabbas Italian Grill",
+      address: "6909 SW 18th St",
+      cuisine: "Italian Food",
+      rating: 3,
+      photoUrl: "http://pennsylvaniarestaurantinspections.com/wp-content/uploads/2020/02/carrabbas-italian-grill-lancaster-restaurant-food-safety-inspection.jpg"
+    }
+    fetch("https://bocacode-intranet-api.web.app/restaurants", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newRestaurants),
+    })
+      .then((response) => response.json())
+      .then((status) => console.log(status))
+  }
 
   return (
     <div className="App App-header">
       <br />
-      <input type="text" onChange={(event) => setText(event.target.value)} value ={text} />
+      <input
+        type="text"
+        onChange={(event) => setText(event.target.value)}
+        value={text}
+      />
       <h3>{text}</h3>
       <br />
       <button onClick={() => handleSubmit()}>Event Handler</button>
+      <br />
+      <button onClick={(event) => sendRestaurant(event)}>Send Post</button>
       {!allRestaurants ? (
         <h2>Loading ..... </h2>
       ) : (
