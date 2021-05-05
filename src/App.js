0993d6e728e04f89cdc76422f8b13c064fe892de
jaRue1 <jaruejohnson@outlook.com>
2021-05-05 13:31:08 -0400

    created a form that sends a post to the api and creates a restrurant

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import React, { useState, useEffect } from "react"
 function App() {
   const [allRestaurants, setAllRestaurants] = useState()
   const [text, setText] = useState("Mutable Text")
+  const [name, setName] = useState("Restaurant Name ")
+  const [address, setAddress] = useState("Restaurant Address")
+  const [cuisine, setCuisine] = useState("Restaurant Cuisine ")
+  const [rating, setRating] = useState("Restaurant Rating")
+  const [photo, setPhoto] = useState("Restaurant Photo")
   useEffect(() => {
     fetch("https://bocacode-intranet-api.web.app/restaurants")
       .then((response) => response.json())
@@ -17,11 +22,11 @@ function App() {
     event.preventDefault()
 
     const newRestaurants = {
-      name: "Carrabbas Italian Grill",
-      address: "6909 SW 18th St",
-      cuisine: "Italian Food",
-      rating: 3,
-      photoUrl: "http://pennsylvaniarestaurantinspections.com/wp-content/uploads/2020/02/carrabbas-italian-grill-lancaster-restaurant-food-safety-inspection.jpg"
+      name: name,
+      address: address,
+      cuisine: cuisine,
+      rating: rating,
+      photoUrl: photo
     }
     fetch("https://bocacode-intranet-api.web.app/restaurants", {
       method: "POST",
@@ -34,23 +39,60 @@ function App() {
 
   return (
     <div className="App App-header">
+      <form action="">
       <br />
+      <label htmlFor="">
+        Example :
       <input
         type="text"
         onChange={(event) => setText(event.target.value)}
-        value={text}
-      />
+        value={text}/>
+       <br />
+       <br />
+       </label>
+       <label>
+         Name : 
+       <input type="text"
+       onChange={(event) => setName(event.target.value)}
+       value={name}/>
+       <br />
+       </label>
+       <label>
+         Address :
+       <input type="text"onChange={(event) => setAddress(event.target.value)}
+       value={address}/>
+       <br />
+       </label>
+       <label>
+         Rating :
+       <input type="text"onChange={(event) => setRating(event.target.value)}
+       value={rating}/>
+       <br />
+       </label>
+       <label>
+         Cuisine :
+       <input type="text"onChange={(event) => setCuisine(event.target.value)}
+       value={cuisine}/>
+       <br />
+       </label>
+       <label>
+         Photo : 
+       <input type="text"onChange={(event) => setPhoto(event.target.value)}
+       value={photo}/>
+       </label>
+       <br/>
+       <br/>
+       <button onClick={(event) => sendRestaurant(event)}>Send Post</button>
+      </form>
       <h3>{text}</h3>
       <br />
       <button onClick={() => handleSubmit()}>Event Handler</button>
-      <br />
-      <button onClick={(event) => sendRestaurant(event)}>Send Post</button>
       {!allRestaurants ? (
         <h2>Loading ..... </h2>
       ) : (
         allRestaurants.map((restaurant, index) => {
           return (
-            <div className="App-header" key={restaurant.id}>
+            <div className="App-header restaurant-card" key={restaurant.id}>
               <h3>Name :{restaurant.name}</h3>
               <span>Address : {restaurant.address}</span>
               <span>Cuisine :{restaurant.cuisine}</span>
